cfc61db26f62c8195b02becf3b22dd85ddcfc81a
Manni <manvir.moore@yahoo.co.uk>
2024-12-18 23:35:43 +0000

    got rid of non-csv files

--- config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from pokemon_v2 import urls as pokemon_v2_urls
-
-# pylint: disable=invalid-name
-
-urlpatterns = [
-    url(r"^", include(pokemon_v2_urls)),
-]
